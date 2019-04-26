35d04fc600e14a3b2f1f66af2419cb96d8c752c6
HernandoMV <vergara@embl.de>
2019-04-26 10:12:54 +0100

    a lot

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -5,6 +5,14 @@
 import numpy as np
 import math
 import re
+from scipy.signal import butter
+from IPython.display import clear_output
+import itertools
+from sklearn import linear_model
+import pandas as pd
+from sklearn.metrics import r2_score
+import matplotlib.pylab as plt
+
 
 def getFirstPulses(pulsevector, timedif):
     """
@@ -85,9 +93,7 @@ def normalizeTrace(trace, traceFrames, samplingfreq, sToZero, normwindow):
     :return: 1D array of x size, normalized
     '''
     # subtract the baseline
-    # calculate how many time events (frames) that is
-    subFrames = int(math.floor(sToZero * samplingfreq.base))
-    subTrace = trace - np.mean(trace[traceFrames<0][-subFrames:])
+    subTrace = translateTrace(trace, traceFrames, samplingfreq, sToZero)
     
     # normalize
     # divide by the minimum of the values within a time window
@@ -96,6 +102,23 @@ def normalizeTrace(trace, traceFrames, samplingfreq, sToZero, normwindow):
     
     return normTrace
 
+
+def translateTrace(trace, traceFrames, samplingfreq, sToZero):
+    '''
+    Translate a trace to zero:
+    subtract the mean of some time before time = 0 (to make it 0)
+    :param trace: 1D array of x size, values of the trace to normalize
+    :param traceFrames: 1D array of x size, range from -a to b
+    :param samplingfreq: in Hz, the frequency of sampling
+    :param sToZero: in seconds, how much time before time 0 to use for subtraction
+    :return: 1D array of x size, translated
+    '''
+    # subtract the baseline
+    # calculate how many time events (frames) that is
+    subFrames = int(math.floor(sToZero * samplingfreq.base))
+    subTrace = trace - np.mean(trace[traceFrames<0][-subFrames:])
+    
+    return subTrace
 
 
 def parseTitlesForDates(expTitles):
@@ -133,4 +156,89 @@ def timeDifferences(listOfDates):
     diftime = np.array(abstimeList) - abstimeList[0]
     
     return diftime
-                      
+               
+       
+def butter_bandpass(lowcut, highcut, fs, order=5, btype='band'):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype=btype)
+    return b, a
+
+
+def butter_lowpass(lowcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    b, a = butter(order, low, btype='low')
+    return b, a
+
+
+def update_progress(progress):
+    bar_length = 20
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+    if progress < 0:
+        progress = 0
+    if progress >= 1:
+        progress = 1
+    block = int(round(bar_length * progress))
+    clear_output(wait = True)
+    text = "Progress: [{0}] {1:.1f}%".format( "#" * block + "-" * (bar_length - block), progress * 100)
+    print(text)
+
+
+def scatterplot_matrix(data, names, **kwargs):
+    """Plots a scatterplot matrix of subplots.  Each row of "data" is plotted
+    against other rows, resulting in a nrows by nrows grid of subplots with the
+    diagonal subplots labeled with "names".  Additional keyword arguments are
+    passed on to matplotlib's "plot" command. Returns the matplotlib figure
+    object containg the subplot grid."""
+    numvars, numdata = data.shape
+    fig, axes = plt.subplots(nrows=numvars, ncols=numvars, figsize=(17,17))
+    fig.subplots_adjust(hspace=0.05, wspace=0.05)
+
+    for ax in axes.flat:
+        # Hide all ticks and labels
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+        # Set up ticks only on one side for the "edge" subplots...
+        if ax.is_first_col():
+            ax.yaxis.set_ticks_position('left')
+        if ax.is_last_col():
+            ax.yaxis.set_ticks_position('right')
+        if ax.is_first_row():
+            ax.xaxis.set_ticks_position('top')
+        if ax.is_last_row():
+            ax.xaxis.set_ticks_position('bottom')
+
+    # Plot the data.
+    for i, j in zip(*np.triu_indices_from(axes, k=1)):
+        for x, y in [(i,j), (j,i)]:
+            axes[x,y].plot(data[x], data[y], **kwargs)
+            
+            # Create linear regression object
+            regr = linear_model.LinearRegression()
+            # Train the model using the training sets
+            regr.fit(pd.DataFrame(data[x]), pd.DataFrame(data[y]))
+            # Make predictions using the testing set
+            #x_pred = pd.DataFrame(np.arange(min(data[x]), max(data[x]), (max(data[x])-min(data[x]))/100))
+            x_pred = pd.DataFrame(data[x])
+            y_pred = regr.predict(x_pred)            
+            axes[x,y].plot(x_pred, y_pred, '-')
+            # The r2 score
+            axes[x,y].text(0.1, 0.2, np.corrcoef(data[x], data[y])[1][0], transform=axes[x,y].transAxes, color = 'blue')
+            axes[x,y].text(0.1, 0.1, r2_score(data[y], y_pred), transform=axes[x,y].transAxes, color = 'blue')
+    # Label the diagonal subplots...
+    for i, label in enumerate(names):
+        axes[i,i].annotate(label, (0.5, 0.5), xycoords='axes fraction',
+                ha='center', va='center')
+
+    # Turn on the proper x or y axes ticks.
+    for i, j in zip(range(numvars), itertools.cycle((-1, 0))):
+        axes[j,i].xaxis.set_visible(True)
+        axes[i,j].yaxis.set_visible(True)
+
+    return fig
